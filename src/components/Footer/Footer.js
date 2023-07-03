a5cf0d7228de0ca556b0582b6118b2e59ecf38d1
0x_ollie <ollieflorendo@gmail.com>
2023-07-03 22:23:17 +0800

    Footer.js

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/0llie16/Portfolio.git'
+      className='link footer__link'
+    >
+      Created by Oliver Florendo
+    </a>
+  </footer>
+)
+
+export default Footer
