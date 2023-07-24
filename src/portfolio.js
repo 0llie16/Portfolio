2cfbbc01ac98e566fc1825cdd438652e65290473
ollie <ollieflorendo@gmail.com>
2023-07-24 14:32:51 +0800

    Portfolio.js

---
+++ src/portfolio.js
@@ -0,0 +1,75 @@
+import aboutpic from "./components/Access/profile-pic.jpg"
+
+const header = {
+  homepage: 'ollieflorendo.com',
+  title: 'JS.',
+}
+
+const about = {
+  photo:aboutpic,
+  name: 'Oliver Florendo',
+  role: 'Front End Developer',
+  description:
+    'I am a self-taught front-end developer with a strong passion for coding and a relentless drive to learn. With a solid foundation in HTML, CSS, JavaScript, and React, I enjoy crafting visually captivating websites and applications that provide seamless user experiences. My portfolio showcases projects that reflect my creativity, problem-solving skills, and dedication to staying current with the ever-changing tech landscape. I am eager to collaborate with fellow developers and take on new challenges as I continue to explore the dynamic world of front-end development. Lets build something amazing together!',
+  resume: 'https://drive.google.com/file/d/1q22Dya9T5HezfmRuvcdCJIF2GvQVaWMY/view?usp=sharing',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/oliver-m-florendo-544461114/',
+    github: 'https://github.com/0llie16',
+  },
+}
+
+const projects = [
+  {
+    name: '',
+    description:
+      '',
+    stack: ['', '', '',''],
+    sourceCode: '',
+    livePreview: '',
+  },
+  {
+    name: '',
+    description:
+      '',
+    stack: ['', '', '','','',''],
+    sourceCode: '',
+    livePreview: '',
+  },
+  {
+    name: '',
+    description:
+      '',
+    stack: ['', '', ''],
+    sourceCode: '',
+    livePreview: '',
+  },
+  {
+    name: '',
+    description:
+      '',
+    stack: ['', '', ''],
+    sourceCode: '',
+    
+  },
+  {
+    name: '',
+    description:
+      '',
+    stack: ['', '', ''],
+    sourceCode: '',
+    livePreview: '',
+  },
+]
+
+const skills = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+]
+
+const contact = {
+  email: 'ollieflorendo@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
